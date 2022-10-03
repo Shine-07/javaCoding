671ae36263be0c0db38275caed655e00abec0f4f
Shine-07 <113321991+Shine-07@users.noreply.github.com>
2022-10-02 22:08:56 -0400

    Add files via upload

---
+++ dimensionalarray.java
@@ -0,0 +1,22 @@
+//problem One
+public class dimensionalarray {
+
+ 
+
+     public static void main(String[] args) {
+
+          String[] words = {"Hello", "my", "name", "is", "the", "barney"};
+
+         for (int i = 0; i < words.length; i++) {
+
+          System.out.print(words[i]);
+
+          if (i != words.length-1) {
+
+              System.out.print(" ");
+             }        
+             }
+
+            System.out.println();
+            }
+            }
